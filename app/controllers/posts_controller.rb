306fc27f097826f025c489726383e792ef11c438
yamotech <nothings.2c9@gmail.com>
2016-06-26 10:49:24 +0900

    new, create機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,18 @@ class PostsController < ApplicationController
   end
 
   def new
+    # インスタンス変数にPostの空のインスタンスを
+    @post = Post.new
   end
 
   def create
+    # インスタンス変数に空枠（ストロングパラメータから取得した値が引数）を渡す
+    @post = Post.new(post_params)
+    # 保存
+    @post.save
+    # showにリダイレクト
+    # redirect_to "/posts/#{@post.id}"
+    redirect_to @post
   end
 
   def edit
@@ -18,5 +27,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  # ストロングパラメータを定義
+  def post_params
+    params.require(:post).permit(:title, :body, :category)
   end
 end
